4da60b9884463bf1c8af28fe483a819b6506f836
Connie Lam <lam_9@hotmail.com>
2018-03-04 18:10:35 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const express = require('express');
+const app = require('connect')();
+const process = require('process');
+
+console.log(__dirname);
+app.use(express.static(__dirname + '/etherdelta.github.io/')); 
+
+app.listen(8080, function () {
+  console.log('EtherDelta is listening on port 8080')
+});
+
+process.on('SIGINT', function() {
+    process.exit();
+});
